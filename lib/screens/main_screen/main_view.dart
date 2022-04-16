7c4c5da1fd874969f486a95440ef4d68ac4044ba
Daniel Nogueira de Oliveira <daniel.n.oliveira@outlook.com>
2022-04-16 20:40:40 -0300

    header bar

--- lib/screens/main_screen/main_view.dart
+++ lib/screens/main_screen/main_view.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 import 'main_view_model.dart';
+import 'widgets/header_bar.dart';
 
 class MainView extends MainViewModel {
   @override
   Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
+    var paddingAppBar = 26.0;
     return Scaffold(
-      body: Container(),
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          children: [const HeaderBar()],
+        ),
+      ),
     );
   }
 }
